4940cce56824602c153d552e5a396fe95dc08fac
skyline-v <65756820+skyline-v@users.noreply.github.com>
2021-08-20 00:18:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-cd ~/.vim && curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim,vimrc\}
-cat vimrc >> ~/.vimrc
+cd ~/.vim && curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim\}
+sed -i "1 source base.vim" ~/.vimrc
+sed -i "2 source gutentags.vim" ~/.vimrc
+sed -i "3 source leaderf.vim" ~/.vimrc
