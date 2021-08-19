9241e70f666c8f7fb16ebbd5dfb56907e23cb01d
skyline-v <65756820+skyline-v@users.noreply.github.com>
2021-08-20 01:24:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-cd ~/.vim && curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim\}
-sed -i "1 source base.vim" ~/.vimrc
-sed -i "2 source gutentags.vim" ~/.vimrc
-sed -i "3 source leaderf.vim" ~/.vimrc
+cd ~/.vim #&& curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim\}
+
+if [ ! -f ~/.vimrc ];then
+  touch ~/.vimrc
+fi
+
+echo "source ~/.vim/base.vim" >> ~/.vimrc
+echo "source ~/.vim/gutentags.vim" >> ~/.vimrc
+echo "source ~/.vim/leaderf.vim" >> ~/.vimrc
+echo "do vim then do PlugInstall..."
