36f5daacb7677a1f2b5bcc9bb4092ad5e3c017f2
skyline-v <65756820+skyline-v@users.noreply.github.com>
2021-08-25 22:57:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cd ~/.vim #&& curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim\}
+cd ~/.vim && curl -fLO https://raw.githubusercontent.com/skyline-v/vimrc/master/\{base.vim,gutentags.vim,leaderf.vim\}
 
 if [ ! -f ~/.vimrc ];then
   touch ~/.vimrc
